9bd0c14449c3b3a0485c69c6ffc90fed3d97adeb
fiqmanan <muhammadsyafiqmanan@gmail.com>
2020-06-03 16:17:18 +0800

    Init
    This is the framework, not yet include the dependency

---
+++ CinemaService/src/main/java/net/cinema/service/CinemaService/CinemaServiceApplication.java
@@ -0,0 +1,13 @@
+package net.cinema.service.CinemaService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaServiceApplication.class, args);
+	}
+
+}
---
+++ CinemaService/src/test/java/net/cinema/service/CinemaService/CinemaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.cinema.service.CinemaService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
